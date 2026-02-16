d4bf9ee9f2560f56b0cbec0de6a315137a0e9e63
Jozef Vaclavik <jozef@hey.com>
2026-02-16 22:36:16 +0400

    fix: SEO fixes

--- app.rb
+++ app.rb
@@ -11,10 +11,20 @@ Trifle::Docs.configure do |config|
   config.register_harvester(Trifle::Docs::Harvester::Markdown)
   config.register_harvester(Trifle::Docs::Harvester::File)
   config.cache = ENV['APP_ENV'] == 'production'
+  config.sitemap_base_url = 'https://docs.trifle.io'
 end
 
 module TrifleIo
   class Docs < Trifle::Docs::App
+    before do
+      next if request.path_info == '/' || !request.path_info.end_with?('/')
+
+      normalized = request.path_info.chomp('/')
+      query = request.query_string.to_s.strip
+      target = query.empty? ? normalized : "#{normalized}?#{query}"
+      redirect target, 301
+    end
+
     not_found do
       erb :not_found, layout: :layout, locals: {
         meta: nil,
